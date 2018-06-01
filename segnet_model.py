b0cf2088f639a6fc1994fd8d037cc517d2b02347
Hiu Chan <hiu_chan@hotmail.com>
2018-06-01 10:52:30 -0400

    Update network

--- segnet_model.py
+++ segnet_model.py
@@ -1,57 +1,51 @@
 from keras.models import Sequential, Model
 from keras.layers import Lambda, Flatten, Dense, Dropout, Layer, Activation, Reshape, Permute
-from keras.layers import Conv2D, Conv2DTranspose, MaxPooling2D, UpSampling2D, ZeroPadding2D
+from keras.layers import Conv2D, Conv2DTranspose, MaxPooling2D, UpSampling2D
 from keras.layers.normalization import BatchNormalization
 
 from helper import *
 
-def get_encoding_layers(kernel = (3, 3), pad = 1, pool_size = 2):
+def get_encoding_layers(kernel = (3, 3), pool_size = 2):
     return [
-#        ZeroPadding2D(padding=(pad,pad)),
         Conv2D(64, kernel, padding='same'),
         BatchNormalization(),
         Activation('relu'),
         MaxPooling2D(pool_size=(pool_size, pool_size)),
 
-#        ZeroPadding2D(padding=(pad,pad)),
         Conv2D(128, kernel, padding='same'),
         BatchNormalization(),
         Activation('relu'),
         MaxPooling2D(pool_size=(pool_size, pool_size)),
 
-#        ZeroPadding2D(padding=(pad,pad)),
         Conv2D(256, kernel, padding='same'),
         BatchNormalization(),
         Activation('relu'),
         MaxPooling2D(pool_size=(pool_size, pool_size)),
 
-#        ZeroPadding2D(padding=(pad,pad)),
         Conv2D(512, kernel, padding='same'),
         BatchNormalization(),
         Activation('relu'),
+        MaxPooling2D(pool_size=(pool_size, pool_size)),
     ]
 
-def get_decoding_layers(kernel = (3, 3), pad = 1, pool_size = 2):
+def get_decoding_layers(kernel = (3, 3), pool_size = 2):
     return[
-#        ZeroPadding2D(padding=(pad,pad)),
+        UpSampling2D(size=(pool_size,pool_size)),
         Conv2D(512, kernel, padding='same'),
         BatchNormalization(),
         Activation('relu'),
 
         UpSampling2D(size=(pool_size,pool_size)),
-#        ZeroPadding2D(padding=(pad,pad)),
         Conv2D(256, kernel, padding='same'),
         BatchNormalization(),
         Activation('relu'),
 
         UpSampling2D(size=(pool_size,pool_size)),
-#        ZeroPadding2D(padding=(pad,pad)),
         Conv2D(128, kernel, padding='same'),
         BatchNormalization(),
         Activation('relu'),
 
         UpSampling2D(size=(pool_size,pool_size)),
-#        ZeroPadding2D(padding=(pad,pad)),
         Conv2D(64, kernel, padding='same'),
         BatchNormalization(),
         Activation('relu'),
@@ -77,101 +71,105 @@ def get_segnet_basic():
 
     return segnet_basic
 
-def get_full_encoding_layers(kernel = (3, 3), pad = 1, pool_size = 2):
+def get_full_encoding_layers(kernel = (3, 3), pool_size = 2):
     return [
-#        ZeroPadding2D(padding=(pad,pad)),
         Conv2D(64, kernel, padding='same'),
         BatchNormalization(),
         Activation('relu'),
-#        ZeroPadding2D(padding=(pad,pad)),
         Conv2D(64, kernel, padding='same'),
         BatchNormalization(),
         Activation('relu'),
         MaxPooling2D(pool_size=(pool_size, pool_size)),
 
-#        ZeroPadding2D(padding=(pad,pad)),
         Conv2D(128, kernel, padding='same'),
         BatchNormalization(),
         Activation('relu'),
-#        ZeroPadding2D(padding=(pad,pad)),
         Conv2D(128, kernel, padding='same'),
         BatchNormalization(),
         Activation('relu'),
         MaxPooling2D(pool_size=(pool_size, pool_size)),
 
-#        ZeroPadding2D(padding=(pad,pad)),
         Conv2D(256, kernel, padding='same'),
         BatchNormalization(),
         Activation('relu'),
-#        ZeroPadding2D(padding=(pad,pad)),
         Conv2D(256, kernel, padding='same'),
         BatchNormalization(),
         Activation('relu'),
-#        ZeroPadding2D(padding=(pad,pad)),
         Conv2D(256, kernel, padding='same'),
         BatchNormalization(),
         Activation('relu'),
         MaxPooling2D(pool_size=(pool_size, pool_size)),
 
-#        ZeroPadding2D(padding=(pad,pad)),
         Conv2D(512, kernel, padding='same'),
         BatchNormalization(),
         Activation('relu'),
-#        ZeroPadding2D(padding=(pad,pad)),
         Conv2D(512, kernel, padding='same'),
         BatchNormalization(),
         Activation('relu'),
-#        ZeroPadding2D(padding=(pad,pad)),
         Conv2D(512, kernel, padding='same'),
         BatchNormalization(),
         Activation('relu'),
+        MaxPooling2D(pool_size=(pool_size, pool_size)),
+
+        Conv2D(512, kernel, padding='same'),
+        BatchNormalization(),
+        Activation('relu'),
+        Conv2D(512, kernel, padding='same'),
+        BatchNormalization(),
+        Activation('relu'),
+        Conv2D(512, kernel, padding='same'),
+        BatchNormalization(),
+        Activation('relu'),
+        MaxPooling2D(pool_size=(pool_size, pool_size)),
     ]
 
-def get_full_decoding_layers(kernel = (3, 3), pad = 1, pool_size = 2):
+def get_full_decoding_layers(kernel = (3, 3), pool_size = 2):
     return[
-#        ZeroPadding2D(padding=(pad,pad)),
+        UpSampling2D(size=(pool_size,pool_size)),
         Conv2D(512, kernel, padding='same'),
         BatchNormalization(),
         Activation('relu'),
-#        ZeroPadding2D(padding=(pad,pad)),
         Conv2D(512, kernel, padding='same'),
         BatchNormalization(),
         Activation('relu'),
-#        ZeroPadding2D(padding=(pad,pad)),
         Conv2D(512, kernel, padding='same'),
         BatchNormalization(),
         Activation('relu'),
 
         UpSampling2D(size=(pool_size,pool_size)),
-#        ZeroPadding2D(padding=(pad,pad)),
+        Conv2D(512, kernel, padding='same'),
+        BatchNormalization(),
+        Activation('relu'),
+        Conv2D(512, kernel, padding='same'),
+        BatchNormalization(),
+        Activation('relu'),
+        Conv2D(512, kernel, padding='same'),
+        BatchNormalization(),
+        Activation('relu'),
+
+        UpSampling2D(size=(pool_size,pool_size)),
         Conv2D(256, kernel, padding='same'),
         BatchNormalization(),
         Activation('relu'),
-#        ZeroPadding2D(padding=(pad,pad)),
         Conv2D(256, kernel, padding='same'),
         BatchNormalization(),
         Activation('relu'),
-#        ZeroPadding2D(padding=(pad,pad)),
         Conv2D(256, kernel, padding='same'),
         BatchNormalization(),
         Activation('relu'),
 
         UpSampling2D(size=(pool_size,pool_size)),
-#        ZeroPadding2D(padding=(pad,pad)),
         Conv2D(128, kernel, padding='same'),
         BatchNormalization(),
         Activation('relu'),
-#        ZeroPadding2D(padding=(pad,pad)),
         Conv2D(128, kernel, padding='same'),
         BatchNormalization(),
         Activation('relu'),
 
         UpSampling2D(size=(pool_size,pool_size)),
-#        ZeroPadding2D(padding=(pad,pad)),
         Conv2D(64, kernel, padding='same'),
         BatchNormalization(),
         Activation('relu'),
-#        ZeroPadding2D(padding=(pad,pad)),
         Conv2D(64, kernel, padding='same'),
         BatchNormalization(),
         Activation('relu'),
